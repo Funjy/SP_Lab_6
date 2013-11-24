bfdd539542e4bc5634baa2aea75fcc5e52526984
Anton <Funjy92@mail.ru>
2013-11-24 23:21:25 +0300

    .

--- SP_Lab_6_client/Chat/ChatWindow.xaml.cs
+++ SP_Lab_6_client/Chat/ChatWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Client_Server_Interface;
 
 namespace SP_Lab_6_client.Chat
 {
---
+++ SP_Lab_6_client/Chat/SupportClasses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+
+namespace SP_Lab_6_client.Chat
+{
+
+    public class Res
+    {
+        public static ResourceManager Rm = new ResourceManager(typeof (LocalRes.SP_Lab_6));
+    }
+
+    public static class AliveInfo
+    {
+        public static CultureInfo CurrentCulture { get; set; }
+
+        public static ChatClient Chat { get; set; }
+
+        static AliveInfo()
+        {
+            CurrentCulture = new CultureInfo("ru");
+        }
+    }
+
+    // ReSharper disable InconsistentNaming
+    public static class FunctionsParameters
+    {
+        public const string GENERAL_MESSAGE = "gnrl_msg";
+    }
+    // ReSharper restore InconsistentNaming
+
+    public delegate void VoidDelegate();
+
+}
